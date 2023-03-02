05b5220b4157ad7de9585ff763a3fbe132f85aa3
Yuriy Durov <u.a.durov@gmail.com>
2023-03-02 19:23:00 +0400

    Add AppInitExtension

---
+++ src/BitzArt.CA.Infrastructure/Extensions/AppInitExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BitzArt.CA;
+
+public static class AppInitExtension
+{
+    public static void Init<T>(this IHost host, Func<IServiceScope, T> action)
+    {
+        using var scope = host.Services.CreateScope();
+        action.Invoke(scope);
+    }
+}
