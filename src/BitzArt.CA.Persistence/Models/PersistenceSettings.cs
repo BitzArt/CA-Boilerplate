7d88fd12fdc54a0544afb79a96c9419626701265
Yuriy Durov <u.a.durov@gmail.com>
2023-03-02 19:01:36 +0400

    Make DbType optional

--- src/BitzArt.CA.Persistence/Models/PersistenceSettings.cs
+++ src/BitzArt.CA.Persistence/Models/PersistenceSettings.cs
@@ -4,7 +4,7 @@ public class PersistenceSettings
 {
     public const string SectionName = "Persistence";
 
-    public required string DbType { get; set; }
+    public string? DbType { get; set; }
     public required string ConnectionString { get; set; }
     public bool AllowDebugOperations { get; set; } = false;
 }
