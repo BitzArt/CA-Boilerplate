bbf293bee49c6f5c57ab331f67ae365966de89fb
Yuriy Durov <u.a.durov@gmail.com>
2023-03-03 14:35:53 +0400

    Add ApiValidateExtension

---
+++ src/BitzArt.CA.Presentation/Validation/ApiValidateExtension.cs
@@ -0,0 +1,20 @@
+﻿using BitzArt;
+
+namespace FluentValidation;
+
+public static class ApiValidateExtension
+{
+    public static void ApiValidate<T>(this IValidator<T> validator, T value, string errorMessage = "Validation failed")
+    {
+        var validationResult = validator.Validate(value);
+        if (!validationResult.IsValid)
+        {
+            var ex = ApiException.BadRequest(errorMessage);
+            ex.Payload.AddData(new
+            {
+                errors = validationResult.Errors.Select(x => x.ErrorMessage)
+            });
+            throw ex;
+        }
+    }
+}
