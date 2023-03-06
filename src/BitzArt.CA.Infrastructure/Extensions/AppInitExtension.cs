7b10f16f5c8e4f145fa6d1a99baca9c0082cfad3
Yuriy Durov <u.a.durov@gmail.com>
2023-03-06 15:04:15 +0400

    Add logging of app initialization time

--- src/BitzArt.CA.Infrastructure/Extensions/AppInitExtension.cs
+++ src/BitzArt.CA.Infrastructure/Extensions/AppInitExtension.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace BitzArt.CA;
 
@@ -7,7 +9,11 @@ public static class AppInitExtension
 {
     public static void Init(this IHost host, Action<IServiceScope> action)
     {
+        var sw = Stopwatch.StartNew();
         using var scope = host.Services.CreateScope();
         action.Invoke(scope);
+        sw.Stop();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<IHost>>();
+        logger.LogInformation("App initialization completed in {ms} ms", sw.ElapsedMilliseconds);
     }
 }
