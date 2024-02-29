cc3c1a5b230da1a2bfc4e4cbec8d7ac2d6b4b87f
Yuriy Durov <u.a.durov@gmail.com>
2024-02-29 19:20:20 +0400

    Add AppDbHealthcheck

---
+++ src/BitzArt.CA.Persistence.EntityFrameworkCore/Healthchecks/AppDbHealthcheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BitzArt.CA.Persistence;
+
+public class AppDbHealthcheck(AppDbContext db) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        var ok = await db.Database.CanConnectAsync(cancellationToken);
+        if (ok) return HealthCheckResult.Healthy();
+        else return HealthCheckResult.Unhealthy();
+    }
+}
