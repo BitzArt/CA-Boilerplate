ee676c9efb618473b03f9b6601d966251960ad6a
Yuriy Durov <u.a.durov@gmail.com>
2025-02-24 16:59:37 +0400

    Add MigrationBuilderExtensions.ExecuteSql

---
+++ src/BitzArt.CA.Persistence.EntityFrameworkCore.Relational/Extensions/MigrationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace Microsoft.EntityFrameworkCore.Migrations;
+
+public static class MigrationBuilderExtensions
+{
+    /// <summary>
+    /// Executes the SQL statement via sp_executesql which does not get validated until runtime.
+    /// </summary>
+    public static OperationBuilder<SqlOperation> ExecuteSql(this MigrationBuilder migrationBuilder, string sql) =>
+        migrationBuilder.Sql($"EXEC sp_executesql N'{sql.Replace("'", "''")}'");
+}
