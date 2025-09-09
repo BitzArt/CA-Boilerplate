4b28d6168e8f5052abb14fa480daf4e7d7742394
Yuriy Durov <yu.durov@gmail.com>
2025-09-09 16:57:02 +0400

    Add Remove overloads with hardDelete option

---
+++ src/BitzArt.CA.Persistence.EntityFrameworkCore/Extensions/DbContextExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BitzArt.CA;
+
+/// <summary>
+/// Extension methods for <see cref="DbContext"/>.
+/// </summary>
+public static class DbContextExtensions
+{
+    /// <inheritdoc cref="DbContext.Remove(object)"/>
+    public static void Remove(this DbContext context, object entity, bool hardDelete = false)
+    {
+        if (hardDelete)
+        {
+            if (entity is not IHardDeletable hardDeletable)
+            {
+                throw new InvalidOperationException($"The entity of type {entity.GetType().Name} does not implement {nameof(IHardDeletable)}.");
+            }
+
+            hardDeletable.HardDelete();
+        }
+
+        context.Remove(entity);
+    }
+
+    /// <inheritdoc cref="DbContext.Remove{TEntity}(TEntity)"/>
+    public static void Remove<T>(this DbContext context, T entity, bool hardDelete = false)
+        where T : IHardDeletable
+    {
+        if (hardDelete)
+        {
+            entity.HardDelete();
+        }
+        
+        context.Remove(entity);
+    }
+}
