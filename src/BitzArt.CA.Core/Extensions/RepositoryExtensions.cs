14733298fc793eea08dac589019d897986f39f12
Yuriy Durov <yu.durov@gmail.com>
2025-09-11 20:22:50 +0400

    Add repository hard remove overload extension

---
+++ src/BitzArt.CA.Core/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace BitzArt.CA;
+
+/// <summary>
+/// Extension methods for <see cref="IRepository{T}"/>.
+/// </summary>
+public static class RepositoryExtensions
+{
+    /// <inheritdoc cref="IRepository{T}.Remove(T)"/>
+    public static void Remove<T>(this IRepository<T> repository, T entity, bool hardDelete = false)
+        where T : class, IHardDeletable
+    {
+        if (hardDelete)
+        {
+            entity.HardDelete();
+        }
+        
+        repository.Remove(entity);
+    }
+}
