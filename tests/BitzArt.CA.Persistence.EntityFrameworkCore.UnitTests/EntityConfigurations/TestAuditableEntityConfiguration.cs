ee6f4b3fa3edb1adec284bba6abe0985ffc3b35c
Yuriy Durov <yu.durov@gmail.com>
2025-09-11 19:01:41 +0400

    Fix class location

---
+++ tests/BitzArt.CA.Persistence.EntityFrameworkCore.UnitTests/EntityConfigurations/TestAuditableEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BitzArt.CA.Persistence;
+
+internal class TestAuditableEntityConfiguration : IEntityTypeConfiguration<TestAuditable>
+{
+    public void Configure(EntityTypeBuilder<TestAuditable> builder)
+    {
+        builder.ToTable("Auditables");
+
+        builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Property(x => x.Name).IsRequired().HasDefaultValue(string.Empty);
+        builder.ConfigureAuditableProperties();
+
+        builder.HasKey(x => x.Id);
+    }
+}
--- tests/BitzArt.CA.Persistence.EntityFrameworkCore.UnitTests/EntityConfigurations/TestSoftDeletableEntityConfiguration.cs
+++ tests/BitzArt.CA.Persistence.EntityFrameworkCore.UnitTests/EntityConfigurations/TestSoftDeletableEntityConfiguration.cs
@@ -15,17 +15,3 @@ internal class TestSoftDeletableEntityConfiguration : IEntityTypeConfiguration<T
         builder.HasKey(x => x.Id);
     }
 }
-
-internal class TestAuditableEntityConfiguration : IEntityTypeConfiguration<TestAuditable>
-{
-    public void Configure(EntityTypeBuilder<TestAuditable> builder)
-    {
-        builder.ToTable("Auditables");
-
-        builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Property(x => x.Name).IsRequired().HasDefaultValue(string.Empty);
-        builder.ConfigureAuditableProperties();
-
-        builder.HasKey(x => x.Id);
-    }
-}
